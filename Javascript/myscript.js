866401049d2cbba6bd2fabf023272229945f3d7f
xtsoulfiretx <alexi.lambrou@gmail.com>
2021-06-02 12:28:50 +0100

    edited array information thanks to Otis

--- Javascript/myscript.js
+++ Javascript/myscript.js
@@ -1,1 +1,51 @@
+// Image Details for displayedImg and LinkedImgs
+const ImageDetails = class {
+    constructor(id, author,url, download_url,) {
+        this.id = id,
+        this.author = author,
+        this.url = url,
+        this.download_url = download_url
+    }
+}
+
+// Fetches a random image
+
+function getImages(){
+    const randomPage = Math.floor(Math.random() * (10) + 1)
+    axios.get(`https://picsum.photos/v2/list?page=${randomPage}&limit=100`)
+            .then(function(response) {
+                pickRandomImage(response.data)
+            }) .catch(err => errorHandler(err))
+
+}
+
+// Picks a random image from the fetched data
+const pickRandomImage = array => {
+    const randomImage = Math.floor(Math.random() * (array.length - 1))
+    const chosenImage = array[randomImage]
+    // const chosenImageLinks = checkLinks(chosenImage)
+    displayedImg = new ImageDetails(
+        chosenImage.id,
+        chosenImage.author,
+        chosenImage.url,
+        chosenImage.download_url,
+        // chosenImageLinks
+    )
+    loadImage()
+    // loadImageDetails()
+}
+
+function loadImage(){
+    $(".test-data").innerHTML = ImageDetails[2];
+}
+//need array's download url
+
+//need ImageDetails function
+
+//need checklinks function
+
+$("#call-test").click(function(){
+    
+});
+
 
